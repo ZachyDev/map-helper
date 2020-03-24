b5393c326f6a53c8f665ec952227b438b96dd9b9
ZachyDev <mosetizachary001@gmail.com>
2020-03-24 18:32:10 +0300

    map helper

---
+++ index.js
@@ -0,0 +1,7 @@
+// create an array of numbers
+let numbers = [1,2,3];
+// define a variable to store the doubled values
+let doubled = numbers.map(function(number){
+    return number*2;
+})
+console.log(doubled);
